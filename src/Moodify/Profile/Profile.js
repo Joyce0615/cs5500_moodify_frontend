216db8cfca74c944e51128ec4055345bed278bf6
Miao Yuan <miaoyuan343@gmail.com>
2024-11-11 16:08:31 -0800

    add upload pic in profile

--- src/Moodify/Profile/Profile.js
+++ src/Moodify/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Profile.css';
 
@@ -10,12 +10,40 @@ function Profile() {
   //   { title: 'Honey Bee (Red River Blue)', artist: 'Blake Shelton' },
   // ];
   const navigate = useNavigate();
-
+  const [file, setFile] = useState(null);
   //handle logout
   const handleLogout = () => {
     localStorage.setItem('isLoggedIn', 'false');
     navigate('/Moodify/Login');
   };
+
+  //upload image
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    const handleUpload = async () => {
+        if (!file) return;
+
+        const formData = new FormData();
+        formData.append("profileImage", file);
+
+        try {
+            const response = await fetch('http://127.0.0.1:5001/api/upload', {
+                method: 'POST',
+                body: formData,
+            });
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log("Image uploaded successfully",data.fileurl);
+            } else {
+                console.error("Failed to upload image");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
 
   return (
     <div className="profile-container">
@@ -23,6 +51,12 @@ function Profile() {
         <div className="profile-image"></div>
         <h2>James S. William</h2>
       </div>
+        {/*upload image */}
+        <div>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+        </div>
+
       <div className="tabs">
         <span className="active-tab">Favourite Songs</span>
         <span>Profile</span>
